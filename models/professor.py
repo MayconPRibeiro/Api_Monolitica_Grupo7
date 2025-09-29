31cad771ee7c7393b30310330c37e56de8901e19
Maycon <maycon_ribeiro10@hotmail.com>
2025-09-29 19:53:23 -0300

    ajustando erro na digitacao

--- models/professor.py
+++ models/professor.py
@@ -7,7 +7,7 @@ class Professor(db.Model):
     nome = db.Column(db.String(100), nullable=False)
     idade = db.Column(db.Integer, nullable=False)
     materia = db.Column(db.String(100), nullable=False)
-    observacoes = db.Column(db.Text, nullabre=True)
+    observacoes = db.Column(db.Text, nullable=True)
 
     # Relacionamento (um professor pode ter várias turmas)
     turmas = db.relationship(
