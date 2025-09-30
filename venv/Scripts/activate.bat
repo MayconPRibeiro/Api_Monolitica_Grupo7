9e2a3d67f2fdc3754808eaa8257d4574bb6f555f
Luis Barbosa <luis.barbosa@faluno.faculdadeimpacta.com.br>
2025-09-30 20:25:24 -0300

    Excluindo database não utilizada

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\2402929\Desktop\Api_Monolitica_Grupo7\venv"
+set "VIRTUAL_ENV=C:\Users\biel_\Downloads\Api_Monolitica_Grupo7\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
